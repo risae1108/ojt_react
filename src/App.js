67277d9ac9dec9d0911cc2b10ea618fd3da6f898
risae <saerisa.1108@gmail.com>
2018-11-01 00:21:16 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import brace from 'brace';
+import AceEditor from 'react-ace';
+import { Button } from 'react-bootstrap';
+import './App.css'
 
-class App extends Component {
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        code: "code"
+      };
+      this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(newValue) {
+    this.state.code = newValue;
+    console.log('change', newValue);
+  }
+
+  /*
+  formatCode() {
+    var submit_code = this.refs.ace.editor.getValue();;
+    var jsonString = JSON.stringify(submit_code);
+    console.log(jsonString);
+  }
+  */
+
   render() {
+
+    const defaultCode = `
+    // 書き換えてみよう！
+    console.log('Hello Online Judge');
+    `;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="container">
+        <h1>SFC Editor</h1>
+        <Button bsStyle="primary" bsSize="large" onClick={this.formatCode}>
+          実行
+        </Button>
+        <AceEditor
+          ref="ace"
+          defaultValue={defaultCode}
+          mode="javascript"
+          theme="monokai"
+          fontSize={16}
+          tabSize={2}
+          wrapEnabled={true}
+          onChange={this.onChange}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{
+              $blockScrolling: Infinity
+          }}
+          style={{marginTop: 10}}
+        />
       </div>
     );
   }
 }
-
-export default App;
