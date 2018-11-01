1e1b3df0cf0b2f494cd359cd26d9f77c69ca1796
risae <saerisa.1108@gmail.com>
2018-11-01 15:59:07 +0900

    Updates

---
+++ precache-manifest.f1c2b40b7f1909ef8bde3b2bad365f2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6459e79fb9b34b7c7439",
+    "url": "/ojt_react/static/js/runtime~main.6459e79f.js"
+  },
+  {
+    "revision": "dc383ce052d8bc38b085",
+    "url": "/ojt_react/static/js/main.dc383ce0.chunk.js"
+  },
+  {
+    "revision": "b4d71487f88c7c457c1e",
+    "url": "/ojt_react/static/js/1.b4d71487.chunk.js"
+  },
+  {
+    "revision": "dc383ce052d8bc38b085",
+    "url": "/ojt_react/static/css/main.c7b44287.chunk.css"
+  },
+  {
+    "revision": "8b8cb9bb7f75d00734ee4d3396cd39b0",
+    "url": "/ojt_react/index.html"
+  }
+];
